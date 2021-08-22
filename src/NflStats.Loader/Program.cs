b470a93aeff642e9408546e4b27a503c5b18e32e
Larry Shields <larry.shields@gmail.com>
2021-08-22 11:14:50 -0400

    Project Setup

---
+++ src/NflStats.Loader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NflStats.Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
