4a1221e0a0641940503dd48374806336780257ff
Larry Shields <larry.shields@gmail.com>
2021-08-23 07:58:19 -0400

    Added Player Transformer with Tests

---
+++ src/NflStats.Loader/Transformers/PlayerTransformer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NflStats.Data.DataObjects;
+using NflStats.Loader.Helpers;
+using NflStats.Loader.Models;
+
+namespace NflStats.Loader.Transformers
+{
+    public class PlayerTransformer : ITransformer<PlayerModel, Player>
+    {
+        public Player Transform(PlayerModel item)
+        {
+            var result = new Player()
+            {
+                Url = item.Url,
+                Name = item.Name
+            };
+
+            if (item.Bio.ContainsKey("Position"))
+            {
+                result.Position = item.Bio.GetValueOrDefault("Position");
+            }
+
+            if (item.Bio.ContainsKey("DOB"))
+            {
+                string value = item.Bio.GetValueOrDefault("DOB");
+                string[] parts = value.Split(" ");
+                if (parts.Length > 1)
+                {
+                    result.Dob = DataHelper.ParseDateTime(parts[0], "MM/dd/yyyy");
+                }
+                else
+                {
+                    result.Dob = DataHelper.ParseDateTime(value, "MM/dd/yyyy");
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/NflStatsLoader.Test/Transformers/PlayerTransformerTest.cs
@@ -0,0 +1,96 @@
+﻿using FluentAssertions;
+using NflStats.Data.DataObjects;
+using NflStats.Loader.Models;
+using NflStats.Loader.Transformers;
+using NUnit.Framework;
+using System;
+
+namespace NflStatsLoader.Test.Transformers
+{
+    public class PlayerTransformerTest
+    {
+        private ITransformer<PlayerModel, Player> transformer;
+
+        [SetUp]
+        public void Setup()
+        {
+            transformer = new PlayerTransformer();
+
+        }
+
+        [Test]
+        public void TransformTest()
+        {
+            var model = new PlayerModel()
+            {
+                Url = "www.google.com",
+                Name = "Jim Smith"
+            };
+            model.Bio.Add("Position", "Quarterback");
+            model.Bio.Add("DOB", "5/21/1996 (25)");
+
+            var result = transformer.Transform(model);
+            
+            result.Dob.Should().Be(new DateTime(1996, 5, 21));
+            result.Name.Should().Be("Jim Smith");
+            result.Position.Should().Be("Quarterback");
+            result.Url.Should().Be("www.google.com");
+        }
+
+        [Test]
+        public void TransformBadDob()
+        {
+            var model = new PlayerModel()
+            {
+                Url = "www.google.com",
+                Name = "Jim Smith"
+            };
+            model.Bio.Add("Position", "Quarterback");
+            model.Bio.Add("DOB", "5/21/1996(25)");
+
+            var result = transformer.Transform(model);
+
+            result.Dob.Should().BeNull();
+            result.Name.Should().Be("Jim Smith");
+            result.Position.Should().Be("Quarterback");
+            result.Url.Should().Be("www.google.com");
+        }
+
+        [Test]
+        public void TransformNoPosition()
+        {
+            var model = new PlayerModel()
+            {
+                Url = "www.google.com",
+                Name = "Jim Smith"
+            };
+            model.Bio.Add("DOB", "5/21/1996(25)");
+
+            var result = transformer.Transform(model);
+
+            result.Dob.Should().BeNull();
+            result.Name.Should().Be("Jim Smith");
+            result.Position.Should().BeNull();
+            result.Url.Should().Be("www.google.com");
+        }
+
+        [Test]
+        public void TransformNoDob()
+        {
+            var model = new PlayerModel()
+            {
+                Url = "www.google.com",
+                Name = "Jim Smith"
+            };
+            model.Bio.Add("Position", "Quarterback");
+
+            var result = transformer.Transform(model);
+
+            result.Dob.Should().BeNull();
+            result.Name.Should().Be("Jim Smith");
+            result.Position.Should().Be("Quarterback");
+            result.Url.Should().Be("www.google.com");
+        }
+
+    }
+}
