2bd01c31ccd55dc891a28b7e59cb8abe91d4ae94
Larry Shields <larry.shields@gmail.com>
2021-08-23 07:35:47 -0400

    Added Data Helper with Tests

---
+++ src/NflStats.Loader/Helpers/DataHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace NflStats.Loader.Helpers
+{
+    public static class DataHelper
+    {
+        
+        public static int ParseInt(string value)
+        {
+            if (int.TryParse(value, out int result))
+            {
+                return result;
+            }
+            return 0;
+        }
+
+        public static long ParseLong(string value)
+        {
+            if (long.TryParse(value, out long result))
+            {
+                return result;
+            }
+            return 0;
+        }
+
+        public static double ParseDouble(string value)
+        {
+            if (double.TryParse(value, out double result))
+            {
+                return result;
+            }
+            return 0;
+        }
+
+        public static DateTime? ParseDateTime(string value, string format)
+        {
+            
+
+            if (!string.IsNullOrEmpty(format) && DateTime.TryParseExact(value, format, new CultureInfo("en-US"), DateTimeStyles.None, out DateTime result))
+            {
+                return result;
+            }
+            return null;
+        }
+
+    }
+}
---
+++ src/NflStatsLoader.Test/Helpers/DataHelperTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NflStats.Loader.Helpers;
+using NUnit.Framework;
+using FluentAssertions;
+
+
+namespace NflStatsLoader.Test.Helpers
+{
+    public class DataHelperTests
+    {
+        [Test]
+        public void ParseIntTest()
+        {
+            var result = DataHelper.ParseInt("25");
+            result.Should().Be(25);
+        }
+
+        [Test]
+        public void ParseLongTest()
+        {
+            var result = DataHelper.ParseLong("568");
+            result.Should().Be(568L);
+        }
+
+        [Test]
+        public void ParseDoubleTest()
+        {
+            var result = DataHelper.ParseDouble("55.6");
+            result.Should().Be(55.6);
+        }
+
+        [Test]
+        public void ParseDateTimeTest()
+        {
+            var result = DataHelper.ParseDateTime("20200506", "yyyyMMdd");
+            result.Should().Be(new DateTime(2020, 5, 6));
+        }
+
+        [Test]
+        public void ParseIntFailureTest()
+        {
+            var result = DataHelper.ParseInt("jeff");
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void ParseLongFailureTest()
+        {
+            var result = DataHelper.ParseLong("jeff");
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void ParseDoubleFailureTest()
+        {
+            var result = DataHelper.ParseDouble("jeff");
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void ParseDateTimeFailureTest()
+        {
+            var result = DataHelper.ParseDateTime("jeff", "yyyyMMdd");
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void ParseDateTimeNoFormatTest()
+        {
+            var result = DataHelper.ParseDateTime("20200506", "");
+            result.Should().BeNull();
+        }
+    }
+}
