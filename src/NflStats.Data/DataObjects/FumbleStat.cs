eda27493bd23b01a47096219063b4987fcce239a
Larry Shields <larry.shields@gmail.com>
2021-08-28 13:15:44 -0400

    Removed/Sorted Usings

--- src/NflStats.Data/DataObjects/FumbleStat.cs
+++ src/NflStats.Data/DataObjects/FumbleStat.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NflStats.Data.DataObjects
+﻿namespace NflStats.Data.DataObjects
 {
     public class FumbleStat : BasePlayerStat
     {
