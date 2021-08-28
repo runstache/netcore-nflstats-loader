fe0ffa09eb27d20a73abb04c61386729ed954c4d
Larry Shields <larry.shields@gmail.com>
2021-08-28 13:11:55 -0400

    Added Fumble Transformer with Tests

---
+++ src/NflStats.Loader/Transformers/FumbleTransformer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NflStats.Data.DataObjects;
+using NflStats.Loader.Models;
+using NflStats.Loader.Helpers;
+
+namespace NflStats.Loader.Transformers
+{
+    public class FumbleTransformer : ITransformer<FumbleModel, FumbleStat>
+    {
+        public FumbleStat Transform(FumbleModel item)
+        {
+            return new FumbleStat()
+            {
+                Fumbles = DataHelper.ParseInt(item.Fumbles),
+                FumblesLost = DataHelper.ParseInt(item.FumblesLost),
+                FumblesRecovered = DataHelper.ParseInt(item.FumblesRecovered)
+            };
+        }
+    }
+}
---
+++ src/NflStatsLoader.Test/Transformers/FumbleTransformerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NflStats.Data.DataObjects;
+using NflStats.Loader.Models;
+using NflStats.Loader.Transformers;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace NflStatsLoader.Test.Transformers
+{
+    public class FumbleTransformerTest
+    {
+        private ITransformer<FumbleModel, FumbleStat> transformer;
+
+        [SetUp]
+        public void Setup()
+        {
+            transformer = new FumbleTransformer();
+        }
+
+        [Test]
+        public void TestTransform()
+        {
+            var model = new FumbleModel()
+            {
+                FumblesRecovered = "1",
+                FumblesLost = "2",
+                Fumbles = "2"
+            };
+            var result = transformer.Transform(model);
+
+            result.Fumbles.Should().Be(2);
+            result.FumblesLost.Should().Be(2);
+            result.FumblesRecovered.Should().Be(1);
+        }
+
+        [Test]
+        public void TestTransformFromJson()
+        {
+            string json = "{\"fumbles\": \"2\",\"fumblesLost\": \"2\",\"fumblesRecovered\": \"0\"}";
+            var settings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+
+            var model = JsonConvert.DeserializeObject<FumbleModel>(json, settings);
+
+            var result = transformer.Transform(model);
+
+            result.Fumbles.Should().Be(2);
+            result.FumblesLost.Should().Be(2);
+            result.FumblesRecovered.Should().Be(0);
+        }
+
+    }
+}
