3cb66131b405e8d35e3d9275c733fee321fe08e9
Larry Shields <larry.shields@gmail.com>
2021-08-28 17:21:18 -0400

    Added Punt Transformer with Tests

---
+++ src/NflStats.Loader/Transformers/PuntingTransformer.cs
@@ -0,0 +1,22 @@
+﻿using NflStats.Data.DataObjects;
+using NflStats.Loader.Helpers;
+using NflStats.Loader.Models;
+
+namespace NflStats.Loader.Transformers
+{
+    public class PuntingTransformer : ITransformer<PuntingModel, KickingStat>
+    {
+        public KickingStat Transform(PuntingModel item)
+        {
+            return new KickingStat()
+            {
+                Punts = DataHelper.ParseInt(item.Punts),
+                PuntInsideTwenty = DataHelper.ParseInt(item.InsideTwenty),
+                PuntLongest = DataHelper.ParseInt(item.LongPunt),
+                PuntTotalYards = DataHelper.ParseInt(item.Yards),
+                AveragePunt = DataHelper.ParseDouble(item.AveragePunt),
+                Touchbacks = DataHelper.ParseInt(item.Touchbacks)
+            };
+        }
+    }
+}
