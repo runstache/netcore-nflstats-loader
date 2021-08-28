07d052727b4da04abe3f691607c6ad17e866ff51
Larry Shields <larry.shields@gmail.com>
2021-08-28 16:36:24 -0400

    Added Interception Transformer with Tests

---
+++ src/NflStats.Loader/Transformers/InterceptionTransformer.cs
@@ -0,0 +1,19 @@
+﻿using NflStats.Data.DataObjects;
+using NflStats.Loader.Helpers;
+using NflStats.Loader.Models;
+
+namespace NflStats.Loader.Transformers
+{
+    public class InterceptionTransformer : ITransformer<InterceptionModel, InterceptionStat>
+    {
+        public InterceptionStat Transform(InterceptionModel item)
+        {
+            return new InterceptionStat()
+            {
+                Interceptions = DataHelper.ParseInt(item.Interceptions),
+                Touchdowns = DataHelper.ParseInt(item.Touchdowns),
+                Yards = DataHelper.ParseInt(item.Yards)
+            };
+        }
+    }
+}
