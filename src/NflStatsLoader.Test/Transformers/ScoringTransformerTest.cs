640a86acafca8e7573a5be27329ec87fe98a16a8
Larry Shields <larry.shields@gmail.com>
2021-08-28 19:55:22 -0400

    Added Scoring Stat Transformer with Tests

---
+++ src/NflStatsLoader.Test/Transformers/ScoringTransformerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NflStats.Data.DataObjects;
+using NflStats.Loader.Models;
+using NflStats.Loader.Transformers;
+using NUnit.Framework;
+
+namespace NflStatsLoader.Test.Transformers
+{
+    public class ScoringTransformerTest
+    {
+        private ITransformer<ScoreValueModel, ScoringStat> transformer;
+
+        [SetUp]
+        public void Setup()
+        {
+            transformer = new ScoringTransformer();
+        }
+
+        [Test]
+        public void TestTransform()
+        {
+            var model = new ScoreValueModel()
+            {
+                Quarter = 2,
+                Points = "42"
+            };
+
+            var result = transformer.Transform(model);
+
+            result.Quarter.Should().Be(2);
+            result.Points.Should().Be(42);
+        }
+
+        public void TestTransformFromJson()
+        {
+            string json = "{\"quarter\": 1,\"points\": \"14\"}";
+            var settings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                MissingMemberHandling = MissingMemberHandling.Ignore
+            };
+
+            var model = JsonConvert.DeserializeObject<ScoreValueModel>(json, settings);
+
+            var result = transformer.Transform(model);
+
+            result.Quarter.Should().Be(1);
+            result.Points.Should().Be(14);
+
+        }
+
+
+    }
+}
