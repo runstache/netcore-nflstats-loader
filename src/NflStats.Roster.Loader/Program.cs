ea50a9bf0efa949070ca622f76b599e167b79142
Larry Shields <larry.shields@gmail.com>
2021-09-11 14:17:28 -0400

    Added New Roster Loader Project

---
+++ src/NflStats.Roster.Loader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NflStats.Roster.Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
