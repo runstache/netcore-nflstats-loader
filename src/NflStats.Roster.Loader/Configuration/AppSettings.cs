513e28de037a43555a26f794f82ff5d718334ca3
Larry Shields <larry.shields@gmail.com>
2021-09-11 15:39:50 -0400

    Added Roster Loader

---
+++ src/NflStats.Roster.Loader/Configuration/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace NflStats.Roster.Loader.Configuration
+{
+    public class AppSettings
+    {
+        public string ConnectionString { get; set; }
+        public string SourceFolder { get; set; }
+    }
+}
---
+++ src/NflStats.Roster.Loader/Models/RosterModel.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace NflStats.Roster.Loader.Models
+{
+    public class RosterModel
+    {
+        [JsonProperty(PropertyName = "team")]
+        public string Team { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "url")]
+        public string Url { get; set; }
+
+        [JsonProperty(PropertyName = "position")]
+        public string Position { get; set; }
+
+    }
+}
